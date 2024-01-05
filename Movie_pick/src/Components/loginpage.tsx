ccee4dc0bbde980f363b92a167d4c4d374cf30da
sainaathkaasa <sainaath4763@gmail.com>
2024-01-05 09:18:04 +0530

    commit

--- Movie_pick/src/Components/loginpage.tsx
+++ Movie_pick/src/Components/loginpage.tsx
@@ -1,95 +1,81 @@
-import React, { useRef, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getlogincred, userRigester } from '../dux/Loginslice'
-import { useNavigate } from 'react-router-dom'
-import '../Styles/loginpage.css'
+import React, { useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getlogincred, userRigester } from '../dux/Loginslice';
+import { useNavigate } from 'react-router-dom';
+import '../Styles/loginpage.css';
 
 const Loginpage = () => {
-    const email = useRef(null)
-    const password = useRef(null)
-    const confpassword = useRef(null)
-    const loginmail = useRef(null)
-    const loginpass = useRef(null)
-    const nav = useNavigate()
-    const [show, setShow] = useState(false)
+    const email = useRef<HTMLInputElement | null>(null);
+    const password = useRef<HTMLInputElement | null>(null);
+    const confpassword = useRef<HTMLInputElement | null>(null);
+    const loginmail = useRef<HTMLInputElement | null>(null);
+    const loginpass = useRef<HTMLInputElement | null>(null);
+    const nav = useNavigate();
+    const [show, setShow] = useState(false);
 
-    const dispatch = useDispatch()
-    const userdb = useSelector(getlogincred)
-        console.log(userdb);
+    const dispatch = useDispatch();
+    const userdb = useSelector(getlogincred);
 
     const handlesubmit = () => {
-        if (email.current.value.length > 0 && password.current.value.length > 0 && password.current.value === confpassword.current.value) {
-            const emailvalue = email.current.value;
-            const passvalue = password.current.value
-            dispatch(userRigester({ emailvalue, passvalue }))
-            email.current.value = ""
-            password.current.value = ''
-            confpassword.current.value = ''
-        }
-    }
-
-    // const singupUser = () => {
-    //     setShow(true);
-    // }
-    const signinUser = () => {
-        // console.log(loginmail.current.value,loginpass.current.value)
-        if(loginmail.current!=null)
-        {
-            if (loginmail.current.value.length > 0 && loginpass.current.value.length > 0){
-                userdb.map((val:any)=>{
-                    console.log(val.emailvalue, val.passvalue)
-                    if(loginmail.current.value === val.emailvalue && loginpass.current.value === val.passvalue){
-                        console.log("success");
-                        nav('/mainpage');
-                    }
-                })
+        if (email.current?.value.length !== undefined && password.current?.value.length !== undefined) {
+            if (email.current?.value.length > 0 && password.current?.value.length > 0 && password.current?.value === confpassword.current?.value && email.current?.value.length != undefined) {
+                const emailvalue = email.current.value;
+                const passvalue = password.current?.value;
+                dispatch(userRigester({ emailvalue, passvalue }));
+                email.current.value = '';
+                password.current.value = '';
+                confpassword.current.value = '';
             }
         }
-        loginmail.current.value = ''
-        loginpass.current.value = ''
-    }
 
-    // const handlebacktotlogin=()=>{
-    //     nav('/')
-    // }
+    };
+
+    const signinUser = () => {
+        if (loginmail.current && loginpass.current) {
+            const enteredEmail = loginmail.current.value;
+            const enteredPassword = loginpass.current.value;
+
+            const userFound = userdb.some((user: any) => user.emailvalue === enteredEmail && user.passvalue === enteredPassword);
+
+            if (userFound) {
+                console.log("Success");
+                nav('/mainpage');
+            } else {
+                console.log("Invalid credentials");
+                // Handle incorrect credentials here (show error message, etc.)
+            }
+
+            loginmail.current.value = '';
+            loginpass.current.value = '';
+        }
+    };
 
     return (
-
         <div className='mainuserUI'>
             <h1>Welcome to the MOVIE RECOMMENDER .com</h1>
-            {
-                !show ? 
-                <form className='login' onSubmit={(e)=>e.preventDefault()}>
+            {!show ? (
+                <form className='login' onSubmit={(e) => e.preventDefault()}>
                     <h1>LOG IN</h1>
                     <label htmlFor='loginmail'>Email</label>
-                    <input type="text" id='loginmail' ref={loginmail}/>
+                    <input type='text' id='loginmail' ref={loginmail} />
                     <label htmlFor='loginpassword'>Password</label>
-                    <input type="password" id='loginpassword'ref={loginpass} />
-                    <button onClick={signinUser}>sign in</button>
-                    {/* <button onClick={singupUser}>sign up</button> */}
-                </form> 
-                : ""
-            }
+                    <input type='password' id='loginpassword' ref={loginpass} />
+                    <button onClick={signinUser}>Sign In</button>
+                </form>
+            ) : null}
 
-            {
-                // show ? 
-                <form className="signup" onSubmit={(e) => e.preventDefault()}>
-                    <h1>SIGN UP</h1>
-                    <label htmlFor='mail' >Email</label>
-                    <input type="text" id='mail' ref={email} />
-                    <label htmlFor='password'>Password</label>
-                    <input type="password" id='password' ref={password} />
-                    <label htmlFor='passwordconf'>Confrom Password</label>
-                    <input type="password" id='passwordconf' ref={confpassword} />
-                    {/* <button onClick={handlebacktotlogin}>back to login</button> */}
-                    <button onClick={handlesubmit}>submit</button>
-                    
-                </form> 
-                // : ""
-            }
-
-
+            <form className='signup' onSubmit={(e) => e.preventDefault()}>
+                <h1>SIGN UP</h1>
+                <label htmlFor='mail'>Email</label>
+                <input type='text' id='mail' ref={email} />
+                <label htmlFor='password'>Password</label>
+                <input type='password' id='password' ref={password} />
+                <label htmlFor='passwordconf'>Confirm Password</label>
+                <input type='password' id='passwordconf' ref={confpassword} />
+                <button onClick={handlesubmit}>Submit</button>
+            </form>
         </div>
-    )
-}
-export default Loginpage
+    );
+};
+
+export default Loginpage;
